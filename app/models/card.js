6b6f32e06dd78b3b06af0ef236520d543fa86397
LeoJian622 <hualuoyu_yks@qq.com>
2017-11-22 16:58:20 +0800

    file data v0.0.1

--- app/models/card.js
+++ app/models/card.js
@@ -1,12 +1,306 @@
-// app/models/card.js
+function cards() {
+	var cards = [{
+		color: "red",
+		direction: -1,
+		step: -1,
+		flag: true
+	}, {
+		color: "blue",
+		direction: -1,
+		step: -1,
+		flag: true
+	}, {
+		color: "yellow",
+		direction: -1,
+		step: -1,
+		flag: true
+	}, {
+		color: "green",
+		direction: -1,
+		step: -1,
+		flag: true
+	}, {
+		color: "purple",
+		direction: -1,
+		step: -1,
+		flag: true
+	}, {
+		color: "colorful",
+		direction: -1,
+		step: -1,
+		flag: true
+	}, {
+		color: "red",
+		direction: -1,
+		step: -1,
+		flag: true
+	}, {
+		color: "blue",
+		direction: -1,
+		step: -1,
+		flag: true
+	}, {
+		color: "yellow",
+		direction: -1,
+		step: -1,
+		flag: true
+	}, {
+		color: "green",
+		direction: -1,
+		step: -1,
+		flag: true
+	}, {
+		color: "purple",
+		direction: -1,
+		step: -1,
+		flag: true
+	}, {
+		color: "colorful",
+		direction: -1,
+		step: -1,
+		flag: true
+	}, {
+		color: "red",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "blue",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "yellow",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "green",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "purple",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "colorful",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "red",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "blue",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "yellow",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "green",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "purple",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "colorful",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "red",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "blue",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "yellow",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "green",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "purple",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "colorful",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "red",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "blue",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "yellow",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "green",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "purple",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "colorful",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "red",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "blue",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "yellow",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "green",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "purple",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "colorful",
+		direction: 1,
+		step: 1,
+		flag: true
+	}, {
+		color: "red",
+		direction: 1,
+		step: 2,
+		flag: true
+	}, {
+		color: "blue",
+		direction: 1,
+		step: 2,
+		flag: true
+	}, {
+		color: "yellow",
+		direction: 1,
+		step: 2,
+		flag: true
+	}, {
+		color: "green",
+		direction: 1,
+		step: 2,
+		flag: true
+	}, {
+		color: "purple",
+		direction: 1,
+		step: 2,
+		flag: true
+	}, {
+		color: "colorful",
+		direction: 0,
+		step: 1,
+		flag: true
+	}, {
+		color: "colorful",
+		direction: 0,
+		step: 1,
+		flag: true
+	}, {
+		color: "colorful",
+		direction: 0,
+		step: 1,
+		flag: true
+	}, {
+		color: "colorful",
+		direction: 0,
+		step: 2,
+		flag: true
+	}, {
+		color: "colorful",
+		direction: 0,
+		step: 2,
+		flag: true
+	}];
 
-var mongoose   = require('mongoose');
-var Schema     = mongoose.Schema;
+	this.showCardstatus = function() {
+		console.log(cards);
+	};
 
-var CardSchema = new Schema({
-	color: 'String',
-	step: 'Number',
-	flag: 'Boolean'
-});
+	this.resetCards = function() {
+		count = 0;
+		for (card in cards) {
+			cards[card].flag = true;
+		}
+		return true;
+	};
 
-module.exports = mongoose.model('Card', CardSchema);
+	var count = 0;
+	this.remainCards = true;
+	this.drawCard = function() {
+		do {
+			card = cards[Math.floor(Math.random() * cards.length)];
+		} while (!card.flag && count <= 52)
+		card.flag = false;
+		count++;
+		if (count == 52) {
+			this.remainCards = false;
+		}
+		return card;
+	};
+};
+
+module.exports = cards;
+
+
+// debug
+// c = new cards();
+// var x = 0;
+// result = c.drawCard();
+// c.showCardstatus();
+// console.log('-----------------------------------');
+// console.log(result);
+// console.log('-----------------------------------');
+// result = c.drawCard();
+// c.showCardstatus();
+// console.log('-----------------------------------');
+// console.log(result);
+// console.log('-----------------------------------');
--- index.js
+++ index.js
@@ -4,90 +4,55 @@
 // =============================================================================
 
 // call the packages
-var express      = require('express');        	// call express
-var app          = express();                 	// define app using express
-var bodyParser   = require('body-parser'); 		// configure app to use bodyParser()
+var express    = require('express'); // call express
+var app        = express(); // define app using express
+var bodyParser = require('body-parser'); // configure app to use bodyParser()
 
-var mongoose     = require('mongoose');
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/tgame',{useMongoClient: true}); // connect to our database
-
-var Card         = require('./app/models/card.js');		//call card model
-
-
+var Card       = require('./app/models/card.js'); //call card model
+var cards      = new Card();
+var User       = require('./app/models/user.js'); //call user model
+var users      = new User();
 app.use(express.static(__dirname + '/public')); // return static source
 
 //get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.use(bodyParser.json());
 
-var port         = process.env.PORT || 3000;		// set port	
+var port = process.env.PORT || 3000; // set port	
 
 // ROUTES FOR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-/*
-Route					HTTP Verb		Description
-/api/cards				GET			Get all the cards.
-/api/cards/:card		GET			Get a single card.
-/api/cards				PUT         Rest all cards statuts.
-*/
-
-// middleware to use for all requests
-// router.use(function(req, res, next) {
-//     // do logging
-//     console.log('Something is happening.');
-//     next(); // make sure we go to the next routes and don't stop here
-// });
-
-
-// test route to make sure everything is working (accessed at GET http://localhost:3000/api)
-// router.get('/', function(req, res) {
-//     res.json({ message: 'hooray! welcome to our api!' });   
-// });
+var router     = express.Router(); // get an instance of the express Router
 
 // more routes for our API will happen here
 // ----------------------------------------------------
 router.route('/cards')
 
-    // create a card (accessed at POST http://localhost:3000/api/cards)
-    .post(function(req, res) {
+	// get single card (accessed at GET http://localhost:3000/api/cards)
+	.get(function(req, res) {
+		res.json(cards.drawCard());
+	})
+	// brocast card data to all
+	.post(function(req, res){
+		res.json({messages:'socket.io brocast!'});
+	})
+	// rest all the cards (accessed at PUT http://localhost:3000/api/cards)
+	.put(function(req, res){
+		if(cards.resetCards()){
+			res.json({messages:'Rest succeed!'});
+		}else{
+			res.json({messages:'Rest failed!'})
+		}
+	});
 
-var card         = new Card();        // create a new instance of the Card model
-card.color       = req.body.color;  // set the cards color (comes from the request)
-card.step        = req.body.step;	 // set the cards step (comes from the request)
-card.flag        = req.body.flag;  // set the cards flag (comes from the request)
-        // save the card and check for errors
-        card.save(function(err) {
-            if (err)
-                res.send(err);
+router.route('/user')
+	//Authentication user and password (accessed at POST http://localhost:3000/api/user)
+	.post(function(req, res){
+		res.json(users.Authentication(req)?{messages:'Login succeed!',status:users.Authentication(req)}:{messages:'Login failed!',status:users.Authentication(req)});
+	})
 
-            res.json({ message: 'Card created!' });
-        });
-    })
-    // get all the cards (accessed at GET http://localhost:3000/api/cards)
-    .get(function(req, res) {
-        Card.find(function(err, cards) {
-            if (err)
-                res.send(err);
-
-            res.json(cards);
-        });
-    });
-
- // on routes that end in /cards/:card_id
-// ----------------------------------------------------
-  router.route('/cards/:card_id')
-
-    // get single the card (accessed at GET http://localhost:3000/api/cards/:card_id)
-    .get(function(req, res){
-    	Card.findById(req.params.card_id, function(err, card){
-    		if (err)
-    			res.send(err);
-    		res.json(card);
-    	});
-    });
 
 
 // REGISTER OUR ROUTES -------------------------------
@@ -99,4 +64,3 @@ app.use('/api', router);
 // =============================================================================
 app.listen(port);
 console.log('Server has been started on port ' + port);
-
